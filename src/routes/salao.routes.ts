b2f383dc8720822486486159ae05e83db4071ed9
Natanael Araújo <natanael@hotmail.co.jp>
2023-06-16 19:20:32 -0300

    rotas salao

--- src/routes/salao.routes.ts
+++ src/routes/salao.routes.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 import { CreateSalaoController } from '../modules/salao/useCases/CreateSalao/CreateSalaoController';
+import { ListAllSalaoServicesController } from '../modules/salao/useCases/ListAllSalaoServices/ListAllSalaoServicesController';
+import { ListSalaoByIdController } from '../modules/salao/useCases/ListSalaoById/ListSalaoByIdController';
 
 const salaoRoutes = Router();
 const createSalaoController = new CreateSalaoController();
+const listAllSalaoServicesController = new ListAllSalaoServicesController();
+const listSalaoByIdController = new ListSalaoByIdController();
 
 salaoRoutes.post("/create", createSalaoController.handle);
+
+salaoRoutes.get('/servicos/:salaoId', listAllSalaoServicesController.handle);
+
+salaoRoutes.get('/:salaoId', listSalaoByIdController.handle);
 
 export default salaoRoutes;
