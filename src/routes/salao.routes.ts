26456f3dfc15a1876b9b3172b0372da2b8542c1b
Natanael Araújo <natanael@hotmail.co.jp>
2023-06-18 07:19:26 -0300

    AlterarStatusSalonCollaborator

--- src/routes/salao.routes.ts
+++ src/routes/salao.routes.ts
@@ -3,16 +3,20 @@ import { Router } from 'express';
 import { CreateSalonController } from '@modules/salon/useCases/CreateSalon/CreateSalonController';
 import { ListAllSalonServicesController } from '@modules/salon/useCases/ListAllSalonServices/ListAllSalonServicesController';
 import { ListSalonByIdController } from '@modules/salon/useCases/ListSalonById/ListSalonByIdController';
+import { AlterarStatusSalonCollaboratorController } from '@modules/salon_collaborators/useCases/AlterarStatusSalonCollaborator/AlterarStatusSalonCollaboratorController';
 
 const salaoRoutes = Router();
 const createSalaoController = new CreateSalonController();
 const listAllSalaoServicesController = new ListAllSalonServicesController();
 const listSalaoByIdController = new ListSalonByIdController();
+const alterarStatusSalonCollaboratorController = new AlterarStatusSalonCollaboratorController()
 
 salaoRoutes.post("/create", createSalaoController.handle);
 
 salaoRoutes.get('/services/:salaoId', listAllSalaoServicesController.handle);
 
 salaoRoutes.get('/:salaoId', listSalaoByIdController.handle);
+
+salaoRoutes.put('/:id', alterarStatusSalonCollaboratorController.handle);
 
 export default salaoRoutes;
