1b54ee7099b240e84cf048d5886f3786fd2a1a2e
Natanael Araújo <natanael@hotmail.co.jp>
2023-07-13 00:09:50 -0300

    Routes fixes

--- src/routes/salaoColaborador.routes.ts
+++ src/routes/salaoColaborador.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 
 import { CreateSalonCollaboratorController } from '@modules/salon_collaborators/useCases/CreateSalonColaborador/CreateSalonCollaboratorController';
+import { ListAllSalonCollaboratorController } from '@modules/salon_collaborators/useCases/ListAllSalonCollaborator/ListAllSalonCollaboratorController';
 
 const salaoColaboradorRoutes = Router();
 const createSalaoColaboradorController = new CreateSalonCollaboratorController();
+const listAllColaboratorController = new ListAllSalonCollaboratorController();
 
-salaoColaboradorRoutes.post("/create", createSalaoColaboradorController.handle);
+salaoColaboradorRoutes.post("/create/", createSalaoColaboradorController.handle);
+
+salaoColaboradorRoutes.get("/listAllSalon", listAllColaboratorController.handle);
 
 export default salaoColaboradorRoutes;
